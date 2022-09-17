673c02f10eff30bf5d8384fe3d6b200c2e2736c0
Lil-Fred <leefredrick16@gmail.com>
2022-09-17 23:56:27 +0300

    loading glb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {useRef, Suspense} from 'react';
+import { Canvas } from '@react-three/fiber';
 import './App.css';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Model(props) {
+  const group = useRef();
+  const {nodes, material} = useGLTF('/carla.glb')
+  return(
+    <group ref={group} {...props} dispose={null}>
+      <mesh
+      geometry={nodes.geometry}
+      material={nodes.material}
+      position={[0,0,0]}
+      />
+    </group>
+  )
 }
 
-export default App;
+function Box() {
+  return(
+  <mesh>
+    <boxBufferGeometry attach="geometry"/>
+    <meshLambertMaterial attach="material" color="hotpink"/>
+  </mesh>
+  )
+}
+
+export default function App(){
+  return(
+
+<Canvas>
+  <Suspense fallback={null}>
+  <OrbitControls/>
+  <ambientLight intensity={0.5}/>
+  <spotLight  position={[10,15,10]} angle={0.3}/>
+  <Model/>
+  <Box/>
+  </Suspense>
+</Canvas>
+  )
+}
